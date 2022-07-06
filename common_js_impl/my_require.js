3dbf3080d25e54504571daf29fd80320134f9279
tomera <tomera@wix.com>
2022-07-07 01:56:28 +0300

    clenaup

--- common_js_impl/my_require.js
+++ common_js_impl/my_require.js
@@ -1,28 +1,18 @@
 const fs = require("fs");
 const vm = require("vm");
 
-const template = (code) => `((exports, require, module) => {
+const wrapAsModule = (code) => `((module, exports) => {
     ${code}
   })`;
 
 function my_require(pathToFile) {
   const module = { exports: {} };
-  console.log(pathToFile);
-  console.log(require.resolve(pathToFile));
   const code = fs.readFileSync(require.resolve(pathToFile)).toString();
-  const codeInModule = template(code);
-  const script = new vm.Script(codeInModule);
-
-  my_require.resolve = require.resolve;
-  script.runInNewContext()(module.exports, my_require, module);
+  const codeAsModule = wrapAsModule(code);
+  const script = new vm.Script(codeAsModule);
+  script.runInNewContext()(module, module.exports);
   return module.exports;
 }
 
 const { add3 } = my_require("./common_js/math.js");
 console.log(add3(2), "should be equal 5");
-
-const _ = my_require("/Users/tomera/module_managment/node_modules/lodash/index.js");
-// const _ = require("lodash");
-console.log(_.add(1, 2));
-
-
